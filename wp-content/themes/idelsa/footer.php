13d6455e7259e0cc9e19fc1a6981a6fa4908ae0d
emergugue <emergugue@gmail.com>
2015-07-19 15:13:33 -0500

    Se borró la carpeta mala de la plantilla y se agregó el screeenshot

--- wp-content/themes/idelsa/footer.php
+++
@@ -1,9 +0,0 @@
-	  	<footer>
-	  		<?php dynamic_sidebar( "sidebar-footer" ); ?>
-	  		<small>Acá viene el Footer</small>
-	  		<?php include TEMPLATEPATH . "/templates/nav-footer.php" ?>
-	  	</footer>
-	  </div>
-  	<?php wp_footer(); ?>
-  </body>
-</html>
